ba8f3b0fc1de25d8482cc87852684013d74a123f
Ruvimal1997 <ruvimal@gmail.com>
2017-05-23 22:20:22 +0530

    23/05/2017 22:19

--- src/forker/game/dto/Deck.java
+++ src/forker/game/dto/Deck.java
@@ -5,10 +5,7 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.security.SecureRandom;
-import java.util.Random;
 import javax.imageio.ImageIO;
-import java.util.ArrayList;
-import java.util.Collections;
 public class Deck
 {
   private Card[] deck;
